ad24e4b067468cf081c2f5556d55d31d5cebe10f
Rayner Tan <raynertanxw@me.com>
2023-08-25 15:16:55 +0800

    Added initial Spring Boot starter project with Spring Web and Spring Boot Dev Tools dependencies added.

---
+++ wingit/src/main/java/com/G2T5203/wingit/WingitApplication.java
@@ -0,0 +1,13 @@
+package com.G2T5203.wingit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WingitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WingitApplication.class, args);
+	}
+
+}
