f3de318403bb09f152b3169b643e3d9c0d013927
Rayner Tan <raynertanxw@me.com>
2023-09-14 12:09:14 +0800

    Created a database init that runs ONLY on dev profile. - Also updated application-prod with the local h2 values and turned some of the stuff off so it would correctly override them from dev.

---
+++ wingit/src/main/java/com/G2T5203/wingit/DatabaseInitializer.java
@@ -0,0 +1,61 @@
+package com.G2T5203.wingit;
+
+import com.G2T5203.wingit.entities.Route;
+import com.G2T5203.wingit.entities.WingitUser;
+import com.G2T5203.wingit.route.RouteRepository;
+import com.G2T5203.wingit.user.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Profile;
+
+import java.sql.Date;
+import java.time.Duration;
+import java.time.LocalDate;
+
+// Class is used only in DEV profile to pre-populate it with data for testing purposes.
+
+public class DatabaseInitializer {
+    private static final Logger logger = LoggerFactory.getLogger(DatabaseInitializer.class);
+    private static void Log(String msg) { logger.debug(msg); }
+    public static void init(ApplicationContext context) {
+        // Initialise WingitUsers
+        UserRepository userRepository = context.getBean(UserRepository.class);
+        Log("[Add WingitUser]: " + userRepository.save(new WingitUser(
+                "goodpassword",
+                "Brandon",
+                "Choy",
+                Date.valueOf(LocalDate.parse("2001-12-04")),
+                "brandon.choy.2037@scis.smu.edu.sg",
+                "+65 8746 3847",
+                "Mr")));
+        Log("[Add WingitUser]: " + userRepository.save(new WingitUser(
+                "password",
+                "Jared",
+                "Hong",
+                Date.valueOf(LocalDate.parse("1996-10-03")),
+                "jared.hong.2034@scis.smu.edu.sg",
+                "+65 8455 0750",
+                "Mrs")));
+
+
+        // Initialise Routes
+        RouteRepository routeRepository = context.getBean(RouteRepository.class);
+        Log("[Add Route]: " + routeRepository.save(new Route(
+                "Singapore",
+                "Taiwan",
+                Duration.ofHours(4).plusMinutes(30) )));
+        Log("[Add Route]: " + routeRepository.save(new Route(
+                "Taiwan",
+                "Singapore",
+                Duration.ofHours(4).plusMinutes(45) )));
+        Log("[Add Route]: " + routeRepository.save(new Route(
+                "Singapore",
+                "Japan",
+                Duration.ofHours(7).plusMinutes(20) )));
+        Log("[Add Route]: " + routeRepository.save(new Route(
+                "Japan",
+                "Singapore",
+                Duration.ofHours(8).plusMinutes(10) )));
+    }
+}
--- wingit/src/main/java/com/G2T5203/wingit/WingitApplication.java
+++ wingit/src/main/java/com/G2T5203/wingit/WingitApplication.java
@@ -1,16 +1,41 @@
 package com.G2T5203.wingit;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Properties;
+import java.util.zip.InflaterOutputStream;
 
 @SpringBootApplication
 public class WingitApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WingitApplication.class, args);
+		ApplicationContext context = SpringApplication.run(WingitApplication.class, args);
+
+		try {
+			org.springframework.core.io.Resource resource = new ClassPathResource("application.properties");
+			Properties props = PropertiesLoaderUtils.loadProperties(resource);
+			String activeProfile = props.getProperty("spring.profiles.active");
+			boolean notProdProfile = !activeProfile.equals("prod");
+			if (notProdProfile) {
+				DatabaseInitializer.init(context);
+			}
+		} catch (IOException e) {
+			System.out.println("ERROR: " + e.getLocalizedMessage());
+		}
 	}
 
 	// create a CORS mapping
