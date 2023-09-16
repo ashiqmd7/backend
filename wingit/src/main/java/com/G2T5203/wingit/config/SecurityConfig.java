900a60ec7c15dcf6742caa02fdb1449a9d99aae4
QiJie53 <limqijie53@gmail.com>
2023-09-16 16:38:12 +0800

    generation of jwt token, secretkey with algorithm, encoder and decoder

--- wingit/src/main/java/com/G2T5203/wingit/config/SecurityConfig.java
+++ wingit/src/main/java/com/G2T5203/wingit/config/SecurityConfig.java
@@ -1,14 +1,27 @@
 package com.G2T5203.wingit.config;
 
+import com.nimbusds.jose.jwk.source.ImmutableSecret;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.crypto.spec.SecretKeySpec;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
@@ -16,6 +29,9 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Value("${jwt.key}")
+    private String jwtKey;
 
     // /api/** authenticated / httpBasic
     @Bean
@@ -49,15 +65,55 @@ public class SecurityConfig {
 //     /permitAll() /private = login with a formLogin
     @Bean
     @Order(3)
+//    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        return http
+//                .csrf(AbstractHttpConfigurer::disable)
+//                .authorizeHttpRequests(auth -> {
+//                    auth.requestMatchers(new AntPathRequestMatcher("/")).permitAll();
+//                    auth.requestMatchers(new AntPathRequestMatcher("/error")).permitAll();
+//                    auth.anyRequest().authenticated();
+//                })
+//                .formLogin(withDefaults())
+//                .build();
+//    }
+
+    // with oauth
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .authorizeHttpRequests(auth -> {
-                    auth.requestMatchers(new AntPathRequestMatcher("/")).permitAll();
-                    auth.requestMatchers(new AntPathRequestMatcher("/error")).permitAll();
-                    auth.anyRequest().authenticated();
-                })
-                .formLogin(withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(AntPathRequestMatcher.antMatcher("/api/auth/token")).hasRole("USER")
+                        .anyRequest().hasAuthority("SCOPE_READ")
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+//                .oauth2ResourceServer((rs) ->
+//                        rs.jwt((jwt) ->jwt.decoder(jwtDecoder()))
+//                )
+                .httpBasic(withDefaults())
                 .build();
-
     }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("sample")
+                        .password("{noop}password")
+                        .authorities("READ","ROLE_USER")
+                        .build());
+    }
+
+    @Bean
+    JwtEncoder jwtEncoder() {
+        return new NimbusJwtEncoder(new ImmutableSecret<>(jwtKey.getBytes()));
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        byte[] bytes = jwtKey.getBytes();
+        SecretKeySpec originalKey = new SecretKeySpec(bytes, 0, bytes.length,"RSA");
+        return NimbusJwtDecoder.withSecretKey(originalKey).macAlgorithm(MacAlgorithm.HS512).build();
+    }
+
+
 }
