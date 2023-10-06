3a1419d89e8574dafec0b8adbb718ba51c098fcb
brandonhjh <brandonhong61@gmail.com>
2023-10-07 01:22:37 +0800

    To cancel a seatListing, user must either is admin or booking user. Note: Added /{username} to the URI!!

--- wingit/src/main/java/com/G2T5203/wingit/seatListing/SeatListingController.java
+++ wingit/src/main/java/com/G2T5203/wingit/seatListing/SeatListingController.java
@@ -2,9 +2,13 @@ package com.G2T5203.wingit.seatListing;
 
 import com.G2T5203.wingit.booking.BookingBadRequestException;
 import com.G2T5203.wingit.entities.SeatListing;
+import com.G2T5203.wingit.user.UserBadRequestException;
 import com.G2T5203.wingit.utils.DateUtils;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -13,6 +17,30 @@ import java.util.Map;
 
 @RestController
 public class SeatListingController {
+    private boolean isAdmin(UserDetails userDetails) { return userDetails.getAuthorities().stream().anyMatch(role -> role.getAuthority().equals("ROLE_ADMIN")); }
+    private boolean isAdmin(Jwt jwt) { return jwt.getClaim("role").equals("ROLE_ADMIN"); }
+    private boolean isNeitherUserNorAdmin(String username, UserDetails userDetails) {
+        boolean isUser = username.equals(userDetails.getUsername());
+        boolean isAdmin = isAdmin(userDetails);
+
+        return (!isUser && !isAdmin);
+    }
+    private boolean isNeitherUserNorAdmin(String username, Jwt jwt) {
+        boolean isUser = username.equals(jwt.getClaim("sub"));
+        boolean isAdmin = isAdmin(jwt);
+
+        return (!isUser && !isAdmin);
+    }
+    private void checkIfNotUserNorAdmin(String username, UserDetails userDetails, Jwt jwt) {
+        if (jwt != null) {
+            if (isNeitherUserNorAdmin(username, jwt)) throw new SeatListingBadRequestException("Not the same user.");
+        } else if (userDetails != null) {
+            if (isNeitherUserNorAdmin(username, userDetails)) throw new SeatListingBadRequestException("Not the same user.");
+        } else {
+            throw new SeatListingBadRequestException("No AuthenticationPrincipal provided for check.");
+        }
+    }
+
     private final SeatListingService service;
 
     public SeatListingController(SeatListingService service) {
@@ -75,8 +103,10 @@ public class SeatListingController {
         }
     }
 
-    @PutMapping(path = "/seatListings/cancelSeatBooking")
-    public SeatListingSimpleJson cancelSeatListingBooking(@Valid @RequestBody SeatListingSimpleJson seatBookingInfo) {
+    @PutMapping(path = "/seatListings/cancelSeatBooking/{username}")
+    public SeatListingSimpleJson cancelSeatListingBooking(@PathVariable String username, @Valid @RequestBody SeatListingSimpleJson seatBookingInfo,
+                                                          @AuthenticationPrincipal UserDetails userDetails, @AuthenticationPrincipal Jwt jwt) {
+        checkIfNotUserNorAdmin(username, userDetails, jwt);
         try {
             SeatListing updatedSeatListing =  service.cancelSeatListingBooking(
                     seatBookingInfo.getPlaneId(),
