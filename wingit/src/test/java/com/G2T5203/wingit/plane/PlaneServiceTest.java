a9812d3661f3d35601e903fffa486d7ebcd7c255
QiJie53 <limqijie53@gmail.com>
2023-10-20 11:21:04 +0800

    clearer line break spacing for readability and minor changes to integration tests

--- wingit/src/test/java/com/G2T5203/wingit/plane/PlaneServiceTest.java
+++ wingit/src/test/java/com/G2T5203/wingit/plane/PlaneServiceTest.java
@@ -24,44 +24,67 @@ public class PlaneServiceTest {
 
     @Test
     void getAllPlanes_Success() {
+        // arrange
         List<Plane> planes = new ArrayList<>();
         when(planeRepository.findAll()).thenReturn(planes);
+
+        // act
         List<Plane> result = planeService.getAllPlanes();
+
+        // verify
         assertEquals(planes, result);
     }
 
     @Test
     void getById_PlaneExists_Success() {
+        // arrange
         String planeId = "Plane123";
         Plane plane = new Plane(planeId, 5, "Plane Model");
         when(planeRepository.findById(planeId)).thenReturn(Optional.of(plane));
+
+        // act
         Plane result = planeService.getById(planeId);
+
+        // verify
         assertNotNull(result);
         assertEquals(planeId, result.getPlaneId());
     }
 
     @Test
     void getById_PlaneNotFound_Failure() {
+        // arrange
         String planeId = "NonExistentPlane";
         when(planeRepository.findById(planeId)).thenReturn(Optional.empty());
+
+        // act
         Plane result = planeService.getById(planeId);
+
+        // verify
         assertNull(result);
     }
 
     @Test
     void createPlane_Success() {
+        // arrange
         Plane newPlane = new Plane("NewPlane", 3, "New Model");
         when(planeRepository.existsById(newPlane.getPlaneId())).thenReturn(false);
         when(planeRepository.save(newPlane)).thenReturn(newPlane);
+
+        // act
         Plane result = planeService.createPlane(newPlane);
+
+        // verify
         assertNotNull(result);
         assertEquals(newPlane.getPlaneId(), result.getPlaneId());
     }
 
     @Test
     void createPlane_PlaneIdExists_Failure() {
+        // arrange
         Plane existingPlane = new Plane("ExistingPlane", 3, "Existing Model");
         when(planeRepository.existsById(existingPlane.getPlaneId())).thenReturn(true);
+
+        // act and verify
         PlaneBadRequestException exception = assertThrows(PlaneBadRequestException.class, () -> {
             planeService.createPlane(existingPlane);
         });
@@ -70,16 +93,22 @@ public class PlaneServiceTest {
 
     @Test
     void deletePlaneById_PlaneExists_Success() {
+        // arrange
         String planeId = "Plane123";
         when(planeRepository.existsById(planeId)).thenReturn(true);
+
+        // act and verify
         assertDoesNotThrow(() -> planeService.deletePlaneById(planeId));
         verify(planeRepository).deleteById(planeId);
     }
 
     @Test
     void deletePlaneById_PlaneNotFound_Failure() {
+        // arrange
         String planeId = "NonExistentPlane";
         when(planeRepository.existsById(planeId)).thenReturn(false);
+
+        // act and verify
         PlaneNotFoundException exception = assertThrows(PlaneNotFoundException.class, () -> {
             planeService.deletePlaneById(planeId);
         });
@@ -88,19 +117,26 @@ public class PlaneServiceTest {
 
     @Test
     void updatePlane_PlaneExists_Success() {
+        // arrange
         Plane updatedPlane = new Plane("UpdatedPlane", 4, "Updated Model");
         when(planeRepository.existsById(updatedPlane.getPlaneId())).thenReturn(true);
         when(planeRepository.save(updatedPlane)).thenReturn(updatedPlane);
+
+        // act
         Plane result = planeService.updatePlane(updatedPlane);
+
+        // verify
         assertNotNull(result);
         assertEquals(updatedPlane.getPlaneId(), result.getPlaneId());
     }
 
     @Test
     void updatePlane_PlaneNotFound_Failure() {
+        // arrange
         Plane nonExistentPlane = new Plane("NonExistentPlane", 3, "NonExistent Model");
         when(planeRepository.existsById(nonExistentPlane.getPlaneId())).thenReturn(false);
 
+        // act and verify
         PlaneNotFoundException exception = assertThrows(PlaneNotFoundException.class, () -> {
             planeService.updatePlane(nonExistentPlane);
         });
--- wingit/src/test/java/com/G2T5203/wingit/route/RouteServiceTest.java
+++ wingit/src/test/java/com/G2T5203/wingit/route/RouteServiceTest.java
@@ -30,30 +30,42 @@ public class RouteServiceTest {
 
     @Test
     void getAllRoutes_Success() {
+        // arrange
         List<Route> routes = Collections.singletonList(new Route());
         when(routeRepository.findAll()).thenReturn(routes);
+
+        // act
         List<Route> result = routeService.getAllRoutes();
+
+        // verify
         verify(routeRepository).findAll();
         assertEquals(routes, result);
     }
 
     @Test
     void getRouteById_Success() {
-        RouteService routeService = new RouteService(routeRepository);
+        // arrange
         int routeId = 1;
         Route expectedRoute = new Route();
         expectedRoute.setRouteId(routeId);
         when(routeRepository.findById(routeId)).thenReturn(Optional.of(expectedRoute));
+
+        // act
         Route result = routeService.getRoute(routeId);
+
+        // verify
         verify(routeRepository).findById(routeId);
         assertEquals(expectedRoute, result);
     }
 
+    // Test for retrieving a route that doesn't exist
     @Test
     void getRouteById_RouteNotExist_Failure() {
-        RouteService routeService = new RouteService(routeRepository);
+        // arrange
         int routeId = 1;
         when(routeRepository.findById(routeId)).thenReturn(Optional.empty());
+
+        // act and verify
         RouteNotFoundException exception = assertThrows(RouteNotFoundException.class, () -> routeService.getRoute(routeId));
         verify(routeRepository).findById(routeId);
         assertEquals("Could not find route 1", exception.getMessage());
@@ -61,11 +73,16 @@ public class RouteServiceTest {
 
     @Test
     void createRoute_Success() {
+        // arrange
         Route newRoute = new Route();
         newRoute.setRouteId(1);
         when(routeRepository.existsById(1)).thenReturn(false);
         when(routeRepository.save(newRoute)).thenReturn(newRoute);
+
+        // act
         Route createdRoute = routeService.createRoute(newRoute);
+
+        // verify
         verify(routeRepository).existsById(1);
         verify(routeRepository).save(newRoute);
         assertEquals(1, createdRoute.getRouteId());
@@ -73,9 +90,12 @@ public class RouteServiceTest {
 
     @Test
     void createRoute_DuplicateRouteId_Failure() {
+        // arrange
         Route newRoute = new Route();
         newRoute.setRouteId(1);
         when(routeRepository.existsById(1)).thenReturn(true);
+
+        // act and verify
         RouteBadRequestException exception = assertThrows(RouteBadRequestException.class, () -> routeService.createRoute(newRoute));
         verify(routeRepository).existsById(1);
         assertEquals("BAD REQUEST: RouteId already exists", exception.getMessage());
@@ -83,15 +103,23 @@ public class RouteServiceTest {
 
     @Test
     void deleteRoute_Success() {
+        // arrange
         when(routeRepository.existsById(1)).thenReturn(true);
+
+        // act
         routeService.deleteRoute(1);
+
+        // verify
         verify(routeRepository).existsById(1);
         verify(routeRepository).deleteById(1);
     }
 
     @Test
     void deleteRoute_RouteNotFound_Failure() {
+        // arrange
         when(routeRepository.existsById(1)).thenReturn(false);
+
+        // act and verify
         RouteNotFoundException exception = assertThrows(RouteNotFoundException.class, () -> routeService.deleteRoute(1));
         verify(routeRepository).existsById(1);
         assertEquals("Could not find route 1", exception.getMessage());
@@ -99,11 +127,16 @@ public class RouteServiceTest {
 
     @Test
     void updateRoute_Success() {
+        // arrange
         Route updatedRoute = new Route();
         updatedRoute.setRouteId(1);
         when(routeRepository.existsById(1)).thenReturn(true);
         when(routeRepository.save(updatedRoute)).thenReturn(updatedRoute);
+
+        // act
         Route result = routeService.updateRoute(updatedRoute);
+
+        // verify
         verify(routeRepository).existsById(1);
         verify(routeRepository).save(updatedRoute);
         assertEquals(1, result.getRouteId());
@@ -111,9 +144,12 @@ public class RouteServiceTest {
 
     @Test
     void updateRoute_RouteNotFound_Failure() {
+        // arrange
         Route updatedRoute = new Route();
         updatedRoute.setRouteId(1);
         when(routeRepository.existsById(1)).thenReturn(false);
+
+        // act and verify
         RouteNotFoundException exception = assertThrows(RouteNotFoundException.class, () -> routeService.updateRoute(updatedRoute));
         verify(routeRepository).existsById(1);
         assertEquals("Could not find route 1", exception.getMessage());
