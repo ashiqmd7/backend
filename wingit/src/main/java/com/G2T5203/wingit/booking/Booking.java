60f76656ba233068177046d573f73cb1d89487b5
Mohamed Ashiq <122281820+ashiqmd7@users.noreply.github.com>
2024-05-23 16:53:18 +0800

    Update Booking.java

--- wingit/src/main/java/com/G2T5203/wingit/booking/Booking.java
+++ wingit/src/main/java/com/G2T5203/wingit/booking/Booking.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
 
+
 import java.time.LocalDateTime;
 import java.util.List;
 
